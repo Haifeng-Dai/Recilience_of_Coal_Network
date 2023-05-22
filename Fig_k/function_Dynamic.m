283fd0acdf7297b478e385ca1ebbac57bd6e02a4
DHF <haifeng_dai_cn@163.com>
2023-05-22 11:08:57 +0800

    add the influence of k_i

---
+++ Fig_k/function_Dynamic.m
@@ -0,0 +1,14 @@
+function f = function_Dynamic(~, x, A, K)
+
+alp = 0.5;
+c = 0.2;
+theta = 0.95;
+delta = 1/5;
+N = size(A,1);
+
+f = alp * x .*(1 - x / K).*(x - c);
+for i = 1 : N
+    for j = 1 : N
+        f(i) = f(i) - (1 - theta) * delta * A(i,j) * x(i) * x(j);
+    end
+end
